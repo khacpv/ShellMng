851727b98a3949be52a92111c46e07118e2a8102
khacpv <khacpv@gmail.com>
2018-03-16 09:37:34 +0700

    Initial Commit

---
+++ ShellMng/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ShellMng
+//
+//  Created by pham khac on 3/16/18.
+//  Copyright © 2018 pham khac. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ShellMng/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShellMng
+//
+//  Created by pham khac on 3/16/18.
+//  Copyright © 2018 pham khac. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
